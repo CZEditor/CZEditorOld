eb1e1b292303119e3d0fcd068ea49a9d64d4400c
relt-1 <michalpulawski1@gmail.com>
2023-01-28 17:47:19 +0100

    Relatively smooth 1080p playback
    Before it was exponentially laggier and laggier, now it maintains ~25 fps

--- avreader.py
+++ avreader.py
@@ -9,24 +9,45 @@ class PyAVSeekableVideoReader:
         self._stream = self._container.streams.video[0]
         self._currentFrame = 0
         self.frame_rate = float(self._stream.average_rate)
+        self._keyframes = []
+        self._container.streams.video[0].thread_type = "AUTO"
+        self._stream.codec_context.skip_frame = "NONKEY"
+        for frame in self._container.decode(self._stream):
+            self._keyframes.append(int(frame.pts*self._stream.time_base*self.frame_rate))
+        self._stream.codec_context.skip_frame = "DEFAULT"
+        self._container.seek(0)
         self._cachedFrame = next(self._container.decode(self._stream)).to_ndarray(format="rgb24")
-    def seek(self,frame:int) -> av.VideoFrame:
-        if(self._currentFrame == frame):
-            return self._cachedFrame
+        
+        
+
+    
+
+    def seekForward(self,frame:int):
         self._currentFrame = frame
-        self._container.seek(int(frame/self._stream.time_base/self.frame_rate),stream=self._stream)
         for decodedFrame in self._container.decode(self._stream):
             if(int(decodedFrame.pts*self._stream.time_base*self.frame_rate) < frame):
                 continue
-            
             return decodedFrame
         else:
             return decodedFrame
 
+    def seek(self,frame:int) -> av.VideoFrame:
+        if(self._currentFrame == frame):
+            return self._cachedFrame
+        self._container.seek(int(frame/self._stream.time_base/self.frame_rate),stream=self._stream)
+        return self.seekForward(frame)
+
     def __getitem__(self,frame:int):
-        if(frame != self._currentFrame):
+        if(frame < self._currentFrame):
             self._cachedFrame = self.seek(frame).to_ndarray(format="rgb24")
-        
+        if(frame > self._currentFrame):
+            for i in self._keyframes:
+                if i < frame and i > self._currentFrame:
+                    self._cachedFrame = self.seek(frame).to_ndarray(format="rgb24")
+                    break
+                elif i > frame:
+                    self._cachedFrame = self.seekForward(frame).to_ndarray(format="rgb24")
+                    break
         return self._cachedFrame
     
     def __len__(self):
